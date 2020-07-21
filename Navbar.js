3111da5e65d77bf91dff298d9d5248ecd7271ebe
noahssark <66339086+noahssark@users.noreply.github.com>
2020-07-21 14:45:33 +0100

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+class Navbar extends Component {
+  render() {
+    return (
+        <p></p>
+    //   <React.Fragment>
+      
+    //     <nav className="navbar navbar-dark bg-dark mb-3">
+    //       <a className="navbar-brand" href="#">
+    //         <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+    //       </a>
+    //     </nav>
+    //   </React.Fragment>
+    );
+  }
+}
+export default Navbar
