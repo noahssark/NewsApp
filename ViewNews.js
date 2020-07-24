964ed251940bd24047a2aed0e4c0be5c233deb0c
noahssark <66339086+noahssark@users.noreply.github.com>
2020-07-24 08:59:47 +0100

    Update ViewNews.js

--- ViewNews.js
+++ ViewNews.js
@@ -3,6 +3,267 @@ import React from 'react';
 function ViewNews() {
   returns(
     <div>
+     [
+    {
+    "source": {
+    "id": "cnn",
+    "name": "CNN"
+    },
+    "author": "James Griffiths, CNN",
+    "title": "US orders closure of Chinese consulate in Houston - CNN",
+    "description": "The United States government has abruptly ordered China to \"cease all operations and events\" at its consulate in Houston, Texas, according to the Chinese Foreign Ministry, in what it called an \"unprecedented escalation\" in recent actions taken by Washington.",
+    "url": "https://www.cnn.com/2020/07/22/politics/china-us-houston-consulate-intl-hnk/index.html",
+    "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/200722163614-us-china-flags-file-beijing-2017-super-tease.jpg",
+    "publishedAt": "2020-07-22T10:16:00Z",
+    "content": "Hong Kong (CNN)The United States government has abruptly ordered China to \"cease all operations and events\" at its consulate in Houston, Texas, according to the Chinese Foreign Ministry, in what it c… [+3310 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Motley Fool"
+    },
+    "author": "Sean Williams",
+    "title": "4 Stocks That Can Hit a $1 Trillion Valuation Next - Motley Fool",
+    "description": "Apple, Microsoft, Amazon, and Alphabet are already part of the trillion-dollar club. These companies could be the next to join.",
+    "url": "https://www.fool.com/investing/2020/07/22/4-stocks-that-can-hit-a-1-trillion-valuation-next.aspx",
+    "urlToImage": "https://g.foolcdn.com/editorial/images/582878/folded-money-cash-invest-retire-social-security-treasury-getty.jpg",
+    "publishedAt": "2020-07-22T10:06:00Z",
+    "content": "For most companies, the coronavirus disease 2019 (COVID-19) pandemic represents one of the most disruptive events they're ever navigated through. But for the market's largest companies, it's represen… [+6641 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CNBC"
+    },
+    "author": "The Associated Press",
+    "title": "Powerful 7.8 quake hits Alaska isles; tsunami threat over - CNBC",
+    "description": "A tsunami advisory was canceled Tuesday evening for coastal areas east and west of the tsunami warning areas.",
+    "url": "https://www.cnbc.com/2020/07/22/powerful-7point8-earthquake-hits-alaska-isles-tsunami-possible.html",
+    "urlToImage": "https://image.cnbcfm.com/api/v1/image/103345647-1593103802877alaskaoil.jpg?v=1593431485",
+    "publishedAt": "2020-07-22T07:40:00Z",
+    "content": "A powerful 7.8 earthquake has struck the Alaska Peninsula and a tsunami warning has been issued.\r\nAccording to the U.S. Geological Survey, the 7.8 magnitude quake struck Tuesday at about 11:12 p.m. P… [+953 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Page Six"
+    },
+    "author": "Joe Tacopino, Brett Bachman",
+    "title": "Kanye West says he's trying to divorce Kim Kardashian in latest Twitter rant - Page Six",
+    "description": "“I been trying to get divorced since Kim met with Meek at the Warldolf for 'prison reform,'” Kanye wrote on Twitter in a reference to rapper Meek Mill.",
+    "url": "https://pagesix.com/2020/07/22/kanye-west-tweets-hes-trying-to-divorce-kim-kardashian/",
+    "urlToImage": "https://pagesix.com/wp-content/uploads/sites/3/2020/07/Kardashian_West.jpg?quality=90&strip=all&w=1200",
+    "publishedAt": "2020-07-22T07:38:00Z",
+    "content": "Kanye West claimed he is trying to divorce his wife, Kim Kardashian, while also accusing her mother, Kris Jenner, of engaging in “white supremacy” in the latest rant from the outspoken rapper.\r\n“I be… [+1390 chars]"
+    },
+    {
+    "source": {
+    "id": "cnn",
+    "name": "CNN"
+    },
+    "author": "Ashley Killough, Ed Lavandera and Kay Jones, CNN",
+    "title": "Texas Covid-19 hot spot is facing a 'tsunami' of patients, overwhelming hospitals - CNN",
+    "description": "Jessica Ortiz said she and her twin brother, Jubal, were inseparable. Even when Jubal lay dead in an open casket with plexiglass over his body -- out of fear he could still be contagious with coronavirus -- she couldn't help but lean down and touch him at his…",
+    "url": "https://www.cnn.com/2020/07/22/us/hidalgo-county-south-texas-covid-19/index.html",
+    "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/200721213346-01-hidalgo-county-south-texas-covid-19-super-tease.jpg",
+    "publishedAt": "2020-07-22T06:38:00Z",
+    "content": "Dallas (CNN)Jessica Ortiz said she and her twin brother, Jubal, were inseparable. Even when Jubal lay dead in an open casket with plexiglass over his body -- out of fear he could still be contagious … [+9058 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Jason Owens",
+    "title": "Serena Williams' 2-year-old daughter Olympia Ohanian listed as part owner of NWSL team - MSN Money",
+    "description": "Serena Williams and Alexis Ohanian have brought their toddler in on a sports ownership stake.",
+    "url": "https://sports.yahoo.com/serena-williams-lists-2-yearold-daughter-olympia-ohanian-as-part-owner-of-nwsl-team-002529349.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/6rdUncWWgSnCJTjmzTk5uQ--~B/aD0xNTA2O3c9MjI1OTtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-images/2020-05/c21d3c80-9adc-11ea-bdde-1e25550dc49e",
+    "publishedAt": "2020-07-22T05:36:00Z",
+    "content": "Listed in the ownership group of the National Womens Soccer Leagues newest team is a 2-year-old.\r\nSerena Williams and husband Alexis Ohanian brought in daughter Olympia Ohanian, Jr. as part of the ow… [+1238 chars]"
+    },
+    {
+    "source": {
+    "id": "reuters",
+    "name": "Reuters"
+    },
+    "author": "Edward Taylor",
+    "title": "How Tesla defined a new era for the global auto industry - Reuters",
+    "description": "Tesla Inc's rapid rise to become the world's most valuable carmaker could mark the start of a new era for the global auto industry, defined by a Silicon Valley approach to software that is overtaking old-school manufacturing know-how.",
+    "url": "https://www.reuters.com/article/us-autos-tesla-newera-insight-idUSKCN24N0GB",
+    "urlToImage": "https://s4.reutersmedia.net/resources/r/?m=02&d=20200722&t=2&i=1526660056&w=1200&r=LYNXNPEG6L08X",
+    "publishedAt": "2020-07-22T05:04:00Z",
+    "content": "FRANKFURT/BEIJING/DETROIT (Reuters) - Tesla Inc’s (TSLA.O) rapid rise to become the world’s most valuable carmaker could mark the start of a new era for the global auto industry, defined by a Silicon… [+8636 chars]"
+    },
+    {
+    "source": {
+    "id": "the-washington-post",
+    "name": "The Washington Post"
+    },
+    "author": "Antonia Farzan, Katie Shepherd",
+    "title": "Live updates: Trump says coronavirus pandemic will ‘get worse before it gets better’ as daily death toll tops 1,000 - The Washington Post",
+    "description": "As the United States reported more than 1,000 coronavirus-related deaths on Tuesday, President Trump offered a grim acknowledgement that the pandemic “will probably, unfortunately, get worse before it gets better.” More than 65,000 new covid-19 cases were rec…",
+    "url": "https://www.washingtonpost.com/nation/2020/07/22/covid-live-updates-us/",
+    "urlToImage": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://d1i4t8bqe7zgj6.cloudfront.net/07-21-2020/t_560e2ba8f4e442d8b8e3b06d6b33672e_name_c9199c68_cba1_11ea_99b0_8426e26d203b.jpg&w=1440",
+    "publishedAt": "2020-07-22T04:32:00Z",
+    "content": "Cornell University suspended its longstanding requirement for seniors to complete a swim test before graduation for those students who will earn their degrees by the 2021 spring semester.\r\nThe test w… [+1000 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "SlashGear"
+    },
+    "author": "Ewdison Then",
+    "title": "Microsoft Teams new features adapt to the new normal of work - SlashGear",
+    "description": "When Microsoft created Teams to compete with then rising communication star Slack, it probably didn’t predict a workplace that would instantly become practically virtual because of a pandemic…",
+    "url": "https://www.slashgear.com/microsoft-teams-new-features-adapt-to-the-new-normal-of-work-21630125/",
+    "urlToImage": "https://cdn.slashgear.com/wp-content/uploads/2020/07/IMAGE-2_ENRICH-Teams.jpg",
+    "publishedAt": "2020-07-22T03:31:00Z",
+    "content": "When Microsoft created Teams to compete with then rising communication star Slack, it probably didn’t predict a workplace that would instantly become practically virtual because of a pandemic. That h… [+1829 chars]"
+    },
+    {
+    "source": {
+    "id": "abc-news",
+    "name": "ABC News"
+    },
+    "author": "William Mansell",
+    "title": "14 people shot outside Chicago funeral: Police - ABC News",
+    "description": "",
+    "url": "https://abcnews.go.com/US/14-people-shot-chicago-funeral-police/story?id=71911574",
+    "urlToImage": "https://s.abcnews.com/images/US/chicago-shooting-200721_1595387588311_hpMain_16x9_992.jpg",
+    "publishedAt": "2020-07-22T03:24:55Z",
+    "content": "At least 14 people were shot outside a Chicago funeral home Tuesday, according to the Chicago Police Department.\r\nAn unknown number of people inside a vehicle shot at multiple people on the street wh… [+885 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Chicago Tribune"
+    },
+    "author": "MARILYNN MARCHIONE",
+    "title": "Coronavirus antibodies fade fast, new research suggests - Chicago Tribune",
+    "description": "New research suggests that antibodies the immune system makes to fight the new coronavirus may only last a few months in people with mild illness.",
+    "url": "https://www.chicagotribune.com/coronavirus/ct-nw-covid-antibodies-20200721-fpharcidfnchpnevrfw4uirrmy-story.html",
+    "urlToImage": "https://www.chicagotribune.com/resizer/9JJp0wAwtsbDpD2OvbqbzyaKMzs=/1200x0/top/cloudfront-us-east-1.images.arcpublishing.com/tronc/KSEE6LTSDIZIIMNYH2UFSXUXM4.aspx",
+    "publishedAt": "2020-07-22T02:45:00Z",
+    "content": null
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Forbes"
+    },
+    "author": "Jamie Carter",
+    "title": "How To See ‘Comet Of The Century’ NEOWISE As Its Passes Closest To Earth On July 22, Then Fades - Forbes",
+    "description": "Catch the comet while you can. It's out of here!",
+    "url": "https://www.forbes.com/sites/jamiecartereurope/2020/07/21/how-to-see-comet-of-the-century-neowise-as-its-passes-closest-to-earth-on-july-22/",
+    "urlToImage": "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f0f03b2db930900063ef962%2F0x0.jpg",
+    "publishedAt": "2020-07-22T02:00:00Z",
+    "content": "The comet Neowise or C/2020 F3 is seen over the Turets, Belarus, 110 kilometers (69 miles) west of ... [+] capital Minsk, early Tuesday, July 14, 2020. (AP Photo/Sergei Grits)\r\nASSOCIATED PRESS\r\nCome… [+3887 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "BBC News"
+    },
+    "author": "Richard Fisher",
+    "title": "Coronavirus: What I learnt in Oxford's vaccine trial - BBC News",
+    "description": "The Covid-19 vaccine developed by Oxford University has shown tantalising results so far. Richard Fisher describes what it’s like to be one of the volunteers in the clinical trials.",
+    "url": "https://www.bbc.com/future/article/20200721-coronavirus-vaccine-trial-what-its-like-to-participate",
+    "urlToImage": "https://ychef.files.bbci.co.uk/live/624x351/p08lfsmt.jpg",
+    "publishedAt": "2020-07-22T01:34:09Z",
+    "content": "Im sitting in a hospital reception, and my breath is fogging my glasses. Minutes ago, I had been running through humid streets, late for my appointment. As doctors and nurses stroll past on their way… [+2261 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Variety"
+    },
+    "author": "Janet W. Lee",
+    "title": "‘Pitch Perfect’ Star Anna Camp Shares Her Coronavirus Symptoms, Urges People to Wear Masks - Variety",
+    "description": "“Pitch Perfect” star Anna Camp revealed she had coronavirus earlier this year, opened up about her symptoms and urged people to wear face masks. In an Instagram post on Tuesday, the actor opened up about how she contracted the virus after not wearing a mask “…",
+    "url": "https://variety.com/2020/film/news/anna-camp-coronavirus-pitch-perfect-1234712889/",
+    "urlToImage": "https://pmcvariety.files.wordpress.com/2020/07/anna-camp.jpg?w=1024",
+    "publishedAt": "2020-07-22T01:31:00Z",
+    "content": "“Pitch Perfect” star Anna Camp revealed she had coronavirus earlier this year, opened up about her symptoms and urged people to wear face masks.\r\nIn an Instagram post on Tuesday, the actor opened up … [+3179 chars]"
+    },
+    {
+    "source": {
+    "id": "fox-news",
+    "name": "Fox News"
+    },
+    "author": "Victor Garcia",
+    "title": "Acting DHS Secretary Chad Wolf calls Biden's criticism of federal law enforcement in Portland 'absurd' - Fox News",
+    "description": "Acting Department of Homeland Security (DHS) Secretary Chad Wolf refuted criticism from presumptive Democratic presidential nominee Joe Biden Tuesday that \"Homeland Security agents\" are \"brutally attacking peaceful protesters\" without \"a clearly defined manda…",
+    "url": "https://www.foxnews.com/media/dhs-secretary-chad-wolf-bidens-criticism-law-enforcement",
+    "urlToImage": "https://cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/147fc996-2516-4b4d-a158-cfce4dd018e2/fed13f6b-0132-47df-be3b-6f203ac9c5c5/1280x720/match/image.jpg",
+    "publishedAt": "2020-07-22T01:06:32Z",
+    "content": "Acting Department of Homeland Security (DHS) Secretary Chad Wolf refuted criticism from presumptive Democratic presidential nominee Joe Biden Tuesday that \"Homeland Security agents\" are \"brutally att… [+2606 chars]"
+    },
+    {
+    "source": {
+    "id": "fox-news",
+    "name": "Fox News"
+    },
+    "author": "Nate Day",
+    "title": "Kate Middleton shares new photos of Prince George for royal's 7th birthday - Fox News",
+    "description": "Tomorrow is a special day for Prince George.",
+    "url": "https://www.foxnews.com/entertainment/kate-middleton-new-photo-prince-george-birthday",
+    "urlToImage": "https://static.foxnews.com/foxnews.com/content/uploads/2019/10/kate-middleton-AP.jpg",
+    "publishedAt": "2020-07-21T23:53:10Z",
+    "content": "Tomorrow is a special day for Prince George.\r\nThe young royal turns 7 on Wednesday, and Kensington Palace celebrated by sharing a pair of photos taken by George's mother, Kate Middleton.\r\nOne of the … [+2053 chars]"
+    },
+    {
+    "source": {
+    "id": "entertainment-weekly",
+    "name": "Entertainment Weekly"
+    },
+    "author": "Tyler Aquilina",
+    "title": "'Glee' star Heather Morris dances to Naya Rivera song in Instagram tribute video - Entertainment Weekly",
+    "description": "Heather Morris posted a video of herself dancing to Naya Rivera's song 'Radio Silence' in tribute to her late 'Glee' costar.",
+    "url": "https://ew.com/tv/heather-morris-dances-to-naya-rivera-song-tribute-video/",
+    "urlToImage": "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B960%2C13%5D&w=2000&h=1047&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F07%2F21%2FGettyImages-138404368.jpg",
+    "publishedAt": "2020-07-21T23:43:00Z",
+    "content": "Heather Morris is paying tribute to her Glee costar Naya Rivera in her own unique fashion.\r\nThe actress posted a video of herself dancing to Rivera's song \"Radio Silence\" on Instagram Monday night, a… [+1415 chars]"
+    },
+    {
+    "source": {
+    "id": "fox-news",
+    "name": "Fox News"
+    },
+    "author": "Nick Givas",
+    "title": "CDC study finds coronavirus infections could be much higher than reported in some cities, states - Fox News",
+    "description": "A study from the Centers for Disease Control (CDC) released on Tuesday claimed coronavirus cases may have been heavily underreported across the country, due to the number of patients who remained asymptomatic or did not require medical treatment.",
+    "url": "https://www.foxnews.com/health/cdc-study-coronavirus-infections-could-be-much-higher-than-reported-in-some-cities-states",
+    "urlToImage": "https://static.foxnews.com/foxnews.com/content/uploads/2020/07/GettyImages-1210839842.jpg",
+    "publishedAt": "2020-07-21T23:28:43Z",
+    "content": "A study from the Centers for Disease Control (CDC) released on Tuesday claimed coronavirus cases may have been heavily underreported across the country, due to the number of patients who remained asy… [+2333 chars]"
+    },
+    {
+    "source": {
+    "id": "usa-today",
+    "name": "USA Today"
+    },
+    "author": "Doyle Rice",
+    "title": "Tropical depression forms in Atlantic Ocean; System in Gulf also bears watching - USA TODAY",
+    "description": "Forecasters Tuesday continued to monitor two separate weather systems for ongoing tropical development.",
+    "url": "https://www.usatoday.com/story/news/nation/2020/07/21/tropical-depression-forms-atlantic-ocean/5481401002/",
+    "urlToImage": "https://www.gannett-cdn.com/presto/2020/07/21/USAT/cea18b98-644b-43b2-a978-f4c521027a0a-two_atl_2d0.png?crop=899,506,x0,y100&width=1600&height=800&fit=bounds",
+    "publishedAt": "2020-07-21T23:18:44Z",
+    "content": "Hurricanes can deal massive damage to homes. Here are a few tips that can help minimize the damage.\r\nAccuweather\r\nStory Highlights\r\n<ul><li>The system in the Atlantic was named Tropical Depression Se… [+2727 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "BBC News"
+    },
+    "author": "https://www.facebook.com/bbcnews",
+    "title": "Amazon-owned firm in Black Lives Matter legal claim - BBC News",
+    "description": "The Amazon-owned supermarket is accused of discriminating against black staff over BLM face masks.",
+    "url": "https://www.bbc.com/news/business-53492868",
+    "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/1141E/production/_113568607_gettyimages-1227712095.jpg",
+    "publishedAt": "2020-07-21T23:02:20Z",
+    "content": "Image copyrightGetty Images\r\nWorkers at Whole Foods are suing the US supermarket for punishing them for wearing Black Lives Matter masks.\r\nThe federal lawsuit says the firm discriminated against blac… [+3364 chars]"}
+]
     </div>
 )
 }
